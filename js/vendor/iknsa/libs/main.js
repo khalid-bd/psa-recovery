cbaf371beabe2380483954d8156a384bd0e4d02f
Khalid Sookia <khalidsookia@gmail.com>
2014-12-05 15:50:43 +0100

    Finilised viewports

--- js/vendor/iknsa/libs/main.js
+++ js/vendor/iknsa/libs/main.js
@@ -279,12 +279,27 @@ jQuery(document).ready(function(){
     {
         center_logo();
     }
+
+    // phones resolutions
     var viewport = $('meta[name="viewport"]');
-    if (!/iPhone/i.test(navigator.userAgent) && screen.width < 640) {
+    if (!/iPhone/i.test(navigator.userAgent) || screen.width < 640 && window.matchMedia("(orientation: portrait)").matches) {
         viewport.attr("content", "initial-scale=0.5, width=device-width, user-scalable = yes");
+        window.addEventListener("orientationchange", function() {
+            viewport.attr("content", "initial-scale=1, width=device-width, user-scalable = yes");
+        }, false);
+    console.log(screen.width);
+    }
+
+    else if (/iPhone/i.test(navigator.userAgent && window.matchMedia("(orientation: portrait)").matches)){
+        viewport.attr("content", "initial-scale=1, width=640, user-scalable = yes");
+        window.addEventListener("orientationchange", function() {
+            viewport.attr("content", "initial-scale=0.5, width=640, user-scalable = yes");
+        }, false);
+    } else {
+        viewport.attr("content", "initial-scale=1, width=device-width, user-scalable = yes");
+    console.log(screen.width);
     }
     console.log(viewport.attr("content"));
-    console.log(screen.width);
 });//End main function all()
 
 // jQuery(document).ready(all());
