01a51879d2dac99cf2972ccd29929fc2fe58ccf8
Khalid Sookia <khalidsookia@gmail.com>
2014-12-12 16:22:38 +0100

    Added touch-to-call class to relevant html pages and updated the js

--- js/vendor/iknsa/libs/main.js
+++ js/vendor/iknsa/libs/main.js
@@ -219,6 +219,10 @@ jQuery(document).ready(function(){
     // Touch to call us
     if($(window).width() < 960) {
         $('.touch-to-call').attr('href', 'tel:0970818283');
+    }else {
+        $('.touch-to-call').click(function(e){
+            e.preventDefault();
+        }).css('cursor', 'default');
     }
 // --------------------------------------------
     // Email input error
